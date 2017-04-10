ea45fd07c875bd6a26658aef89681a65f7fd1302
Danny <dreedbuddy@gmail.com>
2017-04-10 19:58:37 -0400

    Removed bat file
    Batch file not useful for this program

--- make.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo nasm -g -fwin32 charity.asm | %userprofile%\AppData\Local\NASM\nasmpath.bat
-
-echo cl /Zi charity.obj msvcrt.lib legacy_stdio_definitions.lib | %comspec% /k ""C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"" x86
-
-REM return to prompt so we can run the program
-REM cmd
