dd2e1171d862dc3e7cce3af61e5121eb38d542c7
Danny <dreedbuddy@gmail.com>
2017-04-10 18:11:32 -0400

    Boilerplate
    initial commit

---
+++ charity.asm
@@ -0,0 +1,31 @@
+; CpS 230 Team Project Alpha: Team Charity (the greatest of these)
+; Members: Reed, Santana, Yurkin
+;---------------------------------------------------
+; Alpha version program.  Details here: https://protect.bju.edu/cps/courses/cps230/project/project/
+;---------------------------------------------------
+bits 16
+
+; NECESSARY when creating "COM" files (simple DOS executables).
+; (tells NASM take into account the fact that DOS loads
+;  COM files at offset 0x100 in memory)
+org	0x100
+
+section	.text
+	
+	jmp start_threads ; Start the threads
+	
+	
+	
+	
+	
+	
+	.start_threads:
+	
+	
+	
+	.yield:
+
+
+
+section	.data
+
